e631b597b9dc1c8cb90c7ab375138416681b10ff
iNiello <99719850+iNiello@users.noreply.github.com>
2022-05-16 17:11:19 +0200

    feature: #5 #4  first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+
+syntax
+
+include header.php
+include footer.php
+*/
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<title><?php echo Document ?></title>-->
+</head>
+<!--<body>
+    
+    <?php
+        echo "<header>bonjour monde</header>";
+    ?> 
+    
+    
+    <?php
+        echo "<footer>aurevoir monde</footer>";
+    ?>
+</body> -->
+</html>
