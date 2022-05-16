b7ddae251d195ea0936f834764209d5f58e6f999
Diago Dryson <cortezdiago@gmail.com>
2022-05-16 17:03:46 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,17 @@
 <?php
 //vérification si les variables existent
 if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['message'])){
+    $surname = htmlspecialchars($_POST['surname']);
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $email = htmlspecialchars($_POST[̈́'email']);
+
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+    }else{
+        echo "Email non valide";
+    }
     
+}else{
+    heade('Location:index.php');
+    die();
 }
