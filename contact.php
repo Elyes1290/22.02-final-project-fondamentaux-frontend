d1927d741df02f3a134feee3eab53b72038baa2e
Diago Dryson <cortezdiago@gmail.com>
2022-05-17 09:27:28 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,11 @@
 <?php
+    require_once(__DIR__.'/vendor/autoload.php');
+    //voir si ça fonctionne
+    use \Mailjet\Resources;
+    //clé public et clé privé /API
+    define('API_PUBLIC_KEY', 'c91d380675c8bc250175763d942e31bf');
+    define('API_PRIVATE_KEY', 'dab1ebe8cc4c94df3ccf59abce0772cb');
+    $mj = new \Mailjet\Client(API_USER, API_LOGIN,true,['version' => 'v3.1']);
 //vérification si les variables existent
 if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['message'])){
     $surname = htmlspecialchars($_POST['surname']);
@@ -6,8 +13,32 @@ if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['me
     $email = htmlspecialchars($_POST[̈́'email']);
 
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $body = [
+            'Messages' => [
+            [
+                'From' => [
+                'Email' => "EMAIL",
+                'Name' => "NAME"
+                ],
+                'To' => [
+                [
+                    'Email' => "EMAIL",
+                    'Name' => "NAME"
+                ]
+                ],
+                'Subject' => "SUJECT",
+                'TextPart' => 'TEXT EMAIL', 
+                'HTMLPart' => "TEXT EMAIL",
+                'CustomID' => "AppGettingStartedTest"
+            ]
+            ]
+        ];
+            $response = $mj->post(Resources::$Email, ['body' => $body]);
+            $response->success();
+            echo "Email envoyé avec succès !";
 
-    }else{
+    }
+    else{
         echo "Email non valide";
     }
     
