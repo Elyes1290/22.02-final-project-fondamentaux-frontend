cab470279eb73a99350aa4051d0e5c9bd320323c
Diago Dryson <cortezdiago@gmail.com>
2022-05-17 12:00:25 +0200

    update

--- www/contact.php
+++ www/contact.php
@@ -47,3 +47,23 @@ if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['me
     header('Location:index.php');
     die();
 }
+/* function curlPost($url, $data = NULL) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5); //timeout in seconds
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_ENCODING, 'identity');
+    if (!empty($data)) {
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    }
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type"=>"application/json"]);
+    $response = curl_exec($ch);
+    if (curl_error($ch)) {
+    trigger_error('Curl Error:' . curl_error($ch));
+    }
+    curl_close($ch);
+    return $response; */
