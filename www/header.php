12872db1d6cffb50fe936ccf01a3127a70e2bd94
iNiello <99719850+iNiello@users.noreply.github.com>
2022-05-18 00:14:34 +0200

    fix: #4 add fixed top navbar

--- www/header.php
+++ www/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar fixed-top navbar-expand-lg bg-light">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">LOGO</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- www/index.php
+++ www/index.php
@@ -16,11 +16,10 @@ syntax
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 <body>
-<h1>coucou</h1>
 <header>
     <?php include "header.php";?>
 </header>
-    
+    <h1>coucou</h1>
 <footer>
     <?php include "footer.php";?>
 </footer>    
