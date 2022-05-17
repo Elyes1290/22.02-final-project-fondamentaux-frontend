1f5d472e84e098436d9d15bc32c3eb190836ddbc
Vinko Roditi <vinko.d.roditi@gmail.com>
2022-05-17 11:40:04 +0200

    sortTable

---
+++ kanban.js
@@ -0,0 +1,134 @@
+
+const ul = document.getElementById("foo")
+const usersUl = document.getElementById("users-list")
+const tachesUl = document.getElementById("taches-list")
+
+Sortable.create(foo, {
+    group: 'foo',
+    animation: 100,
+    fallbackOnBody: true,
+    ghostClass: "ghost",
+    swapThreshold: 1
+    
+  });
+  Sortable.create(foo, {
+    group: 'foo',
+    animation: 100,
+    fallbackOnBody: true,
+    ghostClass: "ghost",
+    swapThreshold: 1
+    
+  });
+  Sortable.create(foo, {
+    group: 'foo',
+    animation: 100,
+    fallbackOnBody: true,
+    ghostClass: "ghost",
+    swapThreshold: 1
+    
+  });
+  Sortable.create(usersUl, {
+    group: 'foo',
+    animation: 100,
+    fallbackOnBody: true,
+    swapThreshold: 1
+    
+  });
+  Sortable.create(tachesUl, {
+    group: 'foo',
+    animation: 100,
+    fallbackOnBody: true,
+    swapThreshold: 1,
+    ghostClass: "ghost",
+  });
+ 
+
+  let users = ["Sami","Bruno","Loic","Vinko"]
+  let taches = ["tache1", "tache2", "tache3", "tache4", "tache5"]
+  let column1 = {
+    name: "ToDO",
+    color: "red"
+  }
+  let column2 = {
+    name: "inProgress",
+    color: "orange",
+  }
+  let column3 = {
+    name: "finished",
+    color: "green"
+  }
+  let columns = [column1, column2, column3]
+
+  
+  
+  for (const column of columns) {
+    ul.innerHTML += `<div  class="col column-containers  list-group">${column.name}
+    <div id=${column.name} class="list-group-item"> 
+      taches
+    </div>
+    <div/>`
+    Sortable.create(foo, {
+      group: "foo",
+      animation: 100,
+      fallbackOnBody: true,
+      ghostClass: "ghost",
+      swapThreshold: 1
+      
+    });
+    // column.name = document.getElementById(column.name)
+    // Sortable.create(column.name, {
+    //   group: 'foo',
+    //   animation: 100,
+    //   fallbackOnBody: true,
+    //   ghostClass: "ghost"
+      
+  // });
+    
+  }
+  
+  for (const user of users) {
+    usersUl.innerHTML += `<div class="row list-group-item">${user}</div>`
+  }
+  for (const tache of taches) {
+    tachesUl.innerHTML += `<div class="row list-group-item">${tache}</div>`
+  }
+  
+
+// for (const tache of taches) {
+//  let ulNew = document.getElementById(column.name)
+//  ulNew.innerHTML += `<li id=${tache} class="row">${tache}<li/>`
+// } 
+  //id globale
+  const addTache = document.getElementById("ajouter-tache")
+// // const foo = document.getElementById("foo")
+ const inputTache = document.getElementById("input-tache")
+
+// //demande à la base de donéée
+//boucler les éléments de la base
+addTache.addEventListener("click",function(){
+    console.log("clicked")
+    tachesUl.innerHTML += `<div>${inputTache.value}</div>`
+})
+
+const ajouterUser = document.getElementById("ajouter-user")
+const inputUsers = document.getElementById("input-users")
+ajouterUser.addEventListener("click",function(){
+  usersUl.innerHTML += `<div>${inputUsers.value}</div>`
+})
+
+const inputColumn = document.getElementById("input-column")
+const ajouterColumn = document.getElementById("ajouter-column")
+
+ajouterColumn.addEventListener("click",function(){
+  ul.innerHTML += `<div  class="col column-containers  list-group">${inputColumn.value}
+  <div  class="list-group-item"> 
+    taches
+  </div>
+  <div/>`
+})
+// htmlElement.addEventListener('dragend', function(event){
+//     if(event.dataTransfer.dropEffect !== 'none'){
+//         $(this).remove();
+//     }
+// });
+
