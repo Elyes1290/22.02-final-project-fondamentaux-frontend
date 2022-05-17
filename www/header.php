186a8bc720bbc554576bf1356f3181f32c662c90
iNiello <99719850+iNiello@users.noreply.github.com>
2022-05-17 17:10:25 +0200

    update #4 remove 2000 back slash Merci Yonathan

--- www/header.php
+++ www/header.php
@@ -1,27 +1,24 @@
-<?php
-echo "<h3>bonjour</h3>";
-echo "<nav class=\"navbar navbar-expand-lg bg-light\">  
-        <div class=\"container-fluid\">  
-            <a class=\"navbar-brand\" href=\"index.php\">LOGO</a>  
-                <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">  
-                    <span class=\"navbar-toggler-icon\"></span>
-                </button>  
-            <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">  
-                <ul class=\"navbar-nav me-auto mb-2 mb-lg-0\">  
-                    <li class=\"nav-item\">  
-                        <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Accueil</a>  
-                    </li>  
-                    <li class=\"nav-item\">  
-                        <a class=\"nav-link\" href=\"#\">Calendar</a>  
-                    </li>  
-                    <li class=\"nav-item\">  
-                        <a class=\"nav-link\" href=\"#\">Kanban</a>  
-                    </li>  
-                    <li class=\"nav-item\">  
-                        <a class=\"nav-link\" href=\"#\">Inscription</a>  
-                    </li>  
-                </ul>  
-            </div> 
-        </div> 
-    </nav>";
-?>
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">LOGO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class=" navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Calendar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kanban</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inscription</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
