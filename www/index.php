d1ae30d189555f5922db92624ceca14e933cc9a8
iNiello <99719850+iNiello@users.noreply.github.com>
2022-05-18 16:37:25 +0200

    #4 WIP <title> dynamic
    add gitignore
    WIP titre change dynamiquement

--- www/index.php
+++ www/index.php
@@ -3,8 +3,18 @@
 syntax
     include header.php
     include footer.php
+
 */
 ?>
+<?php
+    $title="";
+    if(isset($_POST['acceuil'])){
+        $title="ACCEUIIL";
+    }else{
+        $title="RIEN";
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,7 +22,7 @@ syntax
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--<title><?php echo "Accueil" ?></title>-->
+    <title><?php echo $title ?></title>
     <!-- CSS bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
