aa0de82ba40a819f60f8c174139ba3b1e15c0709
iNiello <99719850+iNiello@users.noreply.github.com>
2022-05-23 11:23:40 +0200

    WIP: dynamic Title

---
+++ www/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+// Place the value from ?page=value in the URL to the variable $page. 
+$page = $_GET['page']; 
+
+// Create an array of the only pages allowed. 
+$pageArray = array( 
+       'index', 
+       'calendar', 
+       'kanban', 
+       'enregistrer',
+         ); 
+
+// If there is no page set, include the default main page. 
+
+if (!$page) { 
+  include('pages/index.php'); 
+} 
+
+// Is $page in the array? 
+$inArray = in_array($page, $pageArray); 
+
+// If so, include it, if not, emit error. 
+ if ($inArray == true) { 
+  include('pages/'. $page .'.php'); 
+}else { 
+  echo 'Umm tisk tisk. You should not be messing with things.'; 
+} 
+
+?>
--- www/header.php
+++ www/header.php
@@ -1,22 +1,25 @@
+<?php
+     include "functions.php";
+?>
 <nav class="navbar fixed-top navbar-expand-lg bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">LOGO</a>
+        <a class="navbar-brand" href="index.php?page=index">LOGO</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class=" navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0" >
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+                    <a class="nav-link active" aria-current="page" href="index.php?page=index">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/calendar.php">Calendar</a>
+                    <a class="nav-link" href="index.php?page=calendar">Calendar</a>
                 </li>
                 <li class="nav-item">
-                     <a class="nav-link" href="index.php?variableName=valueOfVariables">Kanban</a> 
+                     <a class="nav-link" href="index.php?page=kanban">Kanban</a> 
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="enregistrer.php">S'enregistrer</a>
+                    <a class="nav-link" href="index.php?page=enregistrer">S'enregistrer</a>
                 </li>
             </ul>
         </div>
