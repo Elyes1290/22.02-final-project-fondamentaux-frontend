b35441d7d6f77dab38df2211c5e2cadf7be0af7d
iNiello <99719850+iNiello@users.noreply.github.com>
2022-05-23 13:44:02 +0200

    WIP: dynamic title

--- www/home.php
+++ www/home.php
@@ -1,22 +1,24 @@
 <?php
 
+include "data_Home/content.php";
 
 
 // Gestion des blocs
 $blocs = [
     [
-        'title' => 'Bloc1',
+        'title' => $blocTitle1,
         'text' => '4442r2r',
-        'urliimage' => 'http://xxx.jpg',
+        'urliimage' => 'https://images.unsplash.com/photo-1631203924971-ad99ad16a936?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169',
         'color' => '#FF0000'
     ],
     [
-        'title' => 'Bloc2',
+        'title' => $blocTitle2,
         'text' => '4442r2r',
         'urliimage' => 'http://xxx.jpg',
         'color' => '#FF0000'
     ],
-];
+];echo "$blocTitle1";
+
 
 
 
