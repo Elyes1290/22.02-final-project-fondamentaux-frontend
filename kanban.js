c01d245ee2b3c65353bde6ca188967d8ca6de591
Sam <99719826+SamOrion1@users.noreply.github.com>
2022-05-23 12:03:20 +0200

    display modal

--- kanban.js
+++ kanban.js
@@ -1,176 +1,261 @@
-import { newTache } from "./edit.js";
-const ul = document.getElementById("foo")
-const usersUl = document.getElementById("users-list")
-const tachesUl = document.getElementById("taches-list")
-
+const ul = document.getElementById("foo");
+const usersUl = document.getElementById("users-list");
+const tachesUl = document.getElementById("taches-list");
+let newTasks = [];
 
 // les listes nécessaire au début du chargement de la page
 Sortable.create(foo, {
-  group: 'foo',
+  group: "foo",
   animation: 100,
   fallbackOnBody: true,
   ghostClass: "ghost",
-  swapThreshold: 1
-  
+  swapThreshold: 1,
 });
-
 Sortable.create(foo, {
-  group: 'foo',
+  group: "foo",
   animation: 100,
   fallbackOnBody: true,
   ghostClass: "ghost",
-  swapThreshold: 1
-  
+  swapThreshold: 1,
 });
-
 Sortable.create(foo, {
-  group: 'foo',
+  group: "foo",
   animation: 100,
   fallbackOnBody: true,
   ghostClass: "ghost",
-  swapThreshold: 1
-    
+  swapThreshold: 1,
 });
-
 Sortable.create(usersUl, {
-  group: 'foo',
+  group: "foo",
   animation: 100,
   fallbackOnBody: true,
-  swapThreshold: 1
-    
+  swapThreshold: 1,
 });
-
 Sortable.create(tachesUl, {
-  group: 'foo',
+  group: "foo",
   animation: 100,
   fallbackOnBody: true,
   swapThreshold: 1,
   ghostClass: "ghost",
 });
- 
+
+async function postTasks(post) {
+  try {
+    const response = await axios.post("http://localhost:3000/tasks", post);
+    console.log(response)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+async function getusers () {
+  try {
+    const response = await axios.get("http://localhost:3000/users");
+    return await response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getTasks () {
+  try {
+    const response = await axios.get("http://localhost:3000/tasks");
+    return await response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function getColumns () {
+  try {
+    const response = await axios.get("http://localhost:3000/columns");
+    return await response.data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+// Donnés dans modal Sam -------------------------------------------------------------------------------------------------
+
+function generateTasks(item) {
+  console.log(item);
+
+  let p = document.querySelectorAll(".thor");
+  //Add innerHTML depending on index
+                          
+  p[0].textContent = 'Name: ' + item.name
+  p[1].textContent = 'Description: ' + item.description;
+  p[2].textContent = 'Debut ' + item.date_from + ' -' + ' Fin ' + item.date_to;
+  p[3].textContent = "Status: " + item.status;
+}
+
+let taskModalEl = document.getElementById('taskModal')
+taskModalEl.addEventListener('show.bs.modal', generateTasks)
+
+// -------------------------------------------------------------------------------------------------------------------
+
 /// simulation des éléments de retour de la base données
-let users = ["Sami","Bruno","Loic","Vinko"]
+let users = await getusers();
+console.log(users)
+let taches = await getTasks();
+console.log(taches);
+let columns = await getColumns();
+console.log(columns);
 
-let taches = ["tache1", "tache2", "tache3", "tache4", "tache5"]
+// les options permettant de créee des listes
+let containers = null;
+let sortableOption = {
+  group: "foo",
+  animation: 100,
+  fallbackOnBody: true,
+  ghostClass: "ghost",
+  swapThreshold: 1,
+};
 
-let column1 = {
-  name: "ToDO",
-  color: "red"
-}
-let column2 = {
-  name: "inProgress",
-  color: "orange",
-}
-let column3 = {
-  name: "finished",
-  color: "green"
-}
+//myUsersClasses = ["row", "list-group-item"];
+//myTachesClasses = ["row", "list-group-item", "container-item"];
+//myColumnsClasses = ["col", "column-containers", "list-group", "container-item"];
+//myColumnsChildsClasses = ["list-group-item"];
 
-let columns = [column1, column2, column3]
-
-  
 ///Afficher les colonnes provenant futurement les éléments de la base de données
 for (const column of columns) {
   ul.innerHTML += `<div  class="col column-containers  list-group container-item">${column.name}
-  <div id=${column.name} class="list-group-item"> 
-    
-  </div>
-  <div/>`
+    <div id=${column.name} class="list-group-item"> 
+      
+    </div>
+    <div/>`;
+  // let newDiv = document.createElement("div")
+  // newDiv.setAttribute("id",column.name+"a")
+  // newDiv.classList.add(...myColumnsClasses)
+  // let newContent = document.createTextNode(column.name)
+  // newDiv.appendChild(newContent)
+  // attachTo.appendChild(newDiv);
+
+  // let newDiv2 = document.createElement("div")
+  // newDiv2.setAttribute("id",column.name)
+  // newDiv2.classList.add(...myColumnsChildsClasses)
+  // let newContent2 = document.createTextNode("")
+  // newDiv2.appendChild(newContent2)
+  // newDiv.appendChild(newDiv2)
+
   Sortable.create(foo, {
     group: "foo",
     animation: 100,
     fallbackOnBody: true,
     ghostClass: "ghost",
-    swapThreshold: 1
-    
+    swapThreshold: 1,
   });
 }
 
-// les options permettant de créee des listes
-
-let containers = null;
-let sortableOption =  {group: "foo",
-animation: 100,
-fallbackOnBody: true,
-ghostClass: "ghost",
-swapThreshold: 1}
-
-//myUsersClasses = ["row", "list-group-item"]
-//myTachesClasses = ["row","list-group-item","container-item"]
-
 ///Afficher les utilisateurs provenant futurement les éléments de la base de données
-
 for (const user of users) {
   //  usersUl.innerHTML += `<div class="row list-group-item">${user}</div>`
-  //  createDiv(myUsersClasses,user,usersUl)
-  let newDiv = document.createElement("div")
-  newDiv.classList.add("row","list-group-item")
-  let newContent = document.createTextNode(user)
-  newDiv.appendChild(newContent)
-  usersUl.appendChild(newDiv)
+  // createDiv(myUsersClasses,user,usersUl)
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("row", "list-group-item");
+  let newContent = document.createTextNode(user);
+  newDiv.appendChild(newContent);
+  usersUl.appendChild(newDiv);
 }
 
 ///Afficher les taches provenant futurement les éléments de la base de données
-for (const tache of taches) {
-  tachesUl.innerHTML += `<div class="row list-group-item container-item">${tache}</div>`
-  // createDiv(myTachesClasses,tache,tachesUl)
+function createTache(tache) {
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("row", "list-group-item", "container-item");
+  let childButton = document.createElement("button");
+  childButton.value = tache.id;
+  childButton.id = "modal";
+  childButton.className = "btn btn-primary";
+  childButton.setAttribute("type", "button");
+  childButton.setAttribute("data-bs-toggle", "modal");
+  childButton.setAttribute("data-bs-target", "#taskModal");
+  let newContent = document.createElement("div");
+  newContent.textContent = tache.name;
+  newDiv.appendChild(newContent);
+  newDiv.appendChild(childButton);
+  tachesUl.appendChild(newDiv);
 }
 
-//ajouter une tache
-const addTache = document.getElementById("ajouter-tache")
-const inputTache = document.getElementById("input-tache");
-console.log(inputTache);
+for (const tache of taches) {
+  createTache(tache);
+}
 
-let modinput = document.getElementById("mod-tache");
-let modtachebut = document.getElementById("mod-tachebut");
-let nonediv = document.getElementById("nonediv");
-
-addTache.addEventListener("click",function(){
-  //tachesUl.innerHTML += `<div class="row list-group-item container-item">${inputTache.value}</div>`
-  newTache(inputTache, nonediv, tachesUl);
-})
-
-
-modtachebut.addEventListener("click", () => {
-
-  let buttonthor = parseInt(document.getElementById("thor").value)+1;
-  console.log(buttonthor)
-  let input = document.getElementById("inp"+buttonthor);
-  console.log(input);
-  input.textContent = modinput.value;
-})
+const Modal = document.getElementById('taskModal')
+Modal.addEventListener('show.bs.modal', event => {
   
-// ajouter un utilisateur
-const ajouterUser = document.getElementById("ajouter-user")
-const inputUsers = document.getElementById("input-users")
+  const button = event.relatedTarget;
 
-ajouterUser.addEventListener("click",function(){
-  usersUl.innerHTML += `<div class="row list-group-item">${inputUsers.value}</div>`
+  const recipient = button.value;
+  let item;
+
+  if (taches.find(item => item.id == recipient) !== undefined ) {
+    item = taches.find(item => item.id == recipient)
+  } else {
+    item = newTasks.find(item => item.id == recipient)
+  }
+
+  generateTasks(item);
+  
+  const modalTitle = taskModal.querySelector('.modal-title')
+
+  modalTitle.textContent = item.name;
 })
+
+//ajouter une tache
+const addTache = document.getElementById("ajouter-tache");
+
+function randomNum(min, max) {
+  let j = Math.floor(Math.random() * (max - min + 1) + min);
+}
+addTache.addEventListener("click", function () {
+
+  //const inputTache = document.getElementById("input-tache");
+  const dataTask = {
+    id: randomNum(0, 999),
+    name: document.getElementById("input-tache").value,
+    description: "djskdéfjskfjsdkéfsddsdsdsdsdsd",
+    date_from: "17:00",
+    date_to: "18:00",
+    status: "open"
+  }
+
+  newTasks.push(dataTask);
+  
+  postTasks(dataTask);
+  createTache(dataTask);
+});
+
+// ajouter un utilisateur
+const ajouterUser = document.getElementById("ajouter-user");
+const inputUsers = document.getElementById("input-users");
+ajouterUser.addEventListener("click", function () {
+  // usersUl.innerHTML += `<div class="row list-group-item">${inputUsers.value}</div>`
+  createDiv(myUsersClasses, inputUsers.value, usersUl);
+});
 
 // ajouter une colonne
-const inputColumn = document.getElementById("input-column")
-const ajouterColumn = document.getElementById("ajouter-column")
-
-ajouterColumn.addEventListener("click",function(){
+const inputColumn = document.getElementById("input-column");
+const ajouterColumn = document.getElementById("ajouter-column");
+ajouterColumn.addEventListener("click", function () {
   ul.innerHTML += `<div  class="col column-containers  list-group container-item">${inputColumn.value}
   <div  class="list-group-item"> 
   
   </div>
-  <div/>`
-})
+  <div/>`;
+});
 
 // creation de sortable dans les columns
 containers = document.querySelectorAll(".container-item");
-
-  for (var i = 0; i < containers.length; i++) {
-    new Sortable(containers[i],sortableOption );
+for (var i = 0; i < containers.length; i++) {
+  new Sortable(containers[i], sortableOption);
 }
 
+// function permettant d'ajouter une div avec des classes,un contenu et de l'attacher quelque part
 function createDiv(classes, text, attachTo) {
   const newDiv = document.createElement("div");
-  newDiv.classList.add(classes)
-  let newContent = document.createTextNode(text)
-  newDiv.appendChild(newContent)
+  newDiv.classList.add(...classes);
+  let newContent = document.createTextNode(text);
+  newDiv.appendChild(newContent);
   attachTo.appendChild(newDiv);
 }
