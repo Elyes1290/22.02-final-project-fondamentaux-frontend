a2b6c4357063c0d78d078788c79ef1b56710154c
iNiello <99719850+iNiello@users.noreply.github.com>
2022-05-23 15:15:13 +0200

    WIP: update heart in footer

--- www/footer.php
+++ www/footer.php
@@ -1,3 +1,3 @@
-<?php
-echo "<h3>aurevoir</h3>"
-?>
+
+Made with <g-emoji class="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
+<img class="emoji" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png"></g-emoji> by m1,m2,m3,m4,m5,m6.,m7
