7b72a78b20062a8c9ec3e0f9b9a6906947344b15
Xavier <dejuan.xavier@gmail.com>
2022-05-24 12:25:15 +0200

    Calendrier finalisé

--- fullcalendar/calendar.js
+++ fullcalendar/calendar.js
@@ -1,22 +1,19 @@
-// ################## Début Calendar ####################################
+// ################## Début Calendrier #####################################################
 
-document.addEventListener('DOMContentLoaded', async function gettournoi() {
+document.addEventListener('DOMContentLoaded', async function getTasksList() {
 
-// ###################### Variables #####################################
+  // Variables
   let calendarEl = document.getElementById('calendar');
   let namecal = document.getElementById("name")
   let datefromcal = document.getElementById("datefrom")
   let datetocal = document.getElementById("dateto")
   let descriptionModal = document.getElementById('description')
-  // let status = document.getElementById('status')
   let eventsForCalendar = [];
-  
 
-  // ##################### Obtenir les infos depuis l'api #######################
+  // Obtenir les infos depuis l'api
   let eventsList = await getEvents()
 
-
-  // ############ Tableau des éléments qui vont être dans la modale ############  
+  // Tableau des éléments qui vont être dans la modale 
   for (const element of eventsList) {
     let objectToPush = {
       id: element.id,
@@ -24,60 +21,59 @@ document.addEventListener('DOMContentLoaded', async function gettournoi() {
       start: element.date_from,
       end: element.date_to,
       display: element.description + " \n " + element.status,
-    } 
-  // ################ Pousse tous les éléments dans un tableau ################
+    }
+
+    // Pousse les éléments json dans le tableau
     eventsForCalendar.push(objectToPush)
-    console.log(objectToPush)
-    console.log(eventsForCalendar)
   }
-  // ############### Créer un nouvel objet de l'instance FullCalendar #########
+
+  // Créer un nouvel objet de l'instance FullCalendar
   const calendar = new FullCalendar.Calendar(calendarEl, {
 
-  // ############### Boutons du Header ###################################### 
+    // Boutons du Header 
     headerToolbar: {
-      
-      left: "dayGridMonth, dayGridWeek, timeGrid, list", // will normally be on the left. if RTL, will be on the right
+      left: 'prev,next today',
       center: 'title',
-      right: 'today prev,next', // will normally be on the right. if RTL, will be on the left
-
-      // #########################Traduction des boutons ne fonctionne pas #################
-      // today:    "Aujourd'hui",
-      // month:    'Mois',
-      // week:     'Semaine',
-      // day:      'Jour',
-      // list:     'Liste',
-      // ################################################################################
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth',
     },
 
-      // ################# Evénements du tableau #############################
+    // Jours du calendrier en francais
+    locale: 'fr',
+    buttonIcons: true,
+    dayMaxEvents: true, // Crée un lien ouvrant la liste des évéenements à partir d'un certain nombre.  
+
+    // Traduction des bouttons
+    buttonText: {
+      today: "Aujourd'hui",
+      month: 'Mois',
+      week: 'Semaine',
+      day: 'Jour',
+      list: 'Liste',
+    },
+
+    // Evénements du tableau
     events: eventsForCalendar,
-    dateClick: function (arg) {
-      console.log(arg.date.toString()); // use local methods on the native Date Object
-    },
 
-      // ############# Réaction lors de l'événement "clique" + Variables de début de "task" ######################### 
+    // Réaction lors de l'événement "clique" + Variables de début de "task" 
     eventClick: function (info) {
       const dateDebut = new Date(info.event.start);
       const dateFin = new Date(info.event.end);
 
-
-// ################## Display the modal and set the values to the event values. ####################################
+      // Display the modal and set the values to the event values.
       $('.modal').modal('show');
       namecal.innerText = info.event.title
       descriptionModal.innerText = info.event.display
-      // status.innerText = info.event.display
       datefromcal.innerText = "Commence le : " + new Intl.DateTimeFormat('fr-FR', { dateStyle: 'full', timeStyle: 'short' }).format(dateDebut)
       datetocal.innerText = "Se termine le : " + new Intl.DateTimeFormat('fr-FR', { dateStyle: 'full', timeStyle: 'short' }).format(dateFin)
     },
-    editable: true,
-
   });
 
   calendar.render();
+
 });
 
 
-// ################## Get Axios et attente de promesse #################################
+// Get Axios et attente de promesse
 async function getEvents() {
   try {
     let response = await axios.get("https://backend.yonathan.ch/api/tasks/list")
@@ -88,3 +84,4 @@ async function getEvents() {
   }
 }
 
+// ################## Fin Calendrier #####################################################
